62cffc0a8e98949cb23ec5c77cf43e839dd4be77
mod <niklas.fessler@gmail.com>
2015-12-07 11:28:31 +0100

    Initial commit

---
+++ src/bangbang/bangbangclass.java
@@ -0,0 +1,7 @@
+package bangbang;
+
+/**
+ * Created by mod on 12/7/15.
+ */
+public class bangbangclass {
+}
---
+++ src/proportional/proportionalclass.java
@@ -0,0 +1,11 @@
+package proportional;
+
+import com.cyberbotics.webots.controller.DifferentialWheels;
+
+/**
+ * Created by mod on 12/7/15.
+ */
+public class proportionalclass extends DifferentialWheels{
+
+    
+}
