b083b4b157b18d20344a9e21cdc025dbd36af04f
mod <niklas.fessler@gmail.com>
2015-12-17 15:01:39 +0100

    Stop Light

--- BB_WallGrind/src/BB_WallGrind.java
+++ BB_WallGrind/src/BB_WallGrind.java
@@ -4,9 +4,6 @@
  * Created by mod on 12/7/15.
  */
 public class BB_WallGrind {
-    private DistanceSensors distanceSensors;
-    private static final int maxdistanceWest = 3000;
-    private static final int maxdistanceNorth = 2700;
     private static int TIME_STEP = 16;
     private Robot robot;
 
@@ -23,8 +20,6 @@ public class BB_WallGrind {
 
     public void run() {
         boolean wallfound = false;
-
-        int west = (int) robot.getDSSensor("W").getValue();
         while (robot.step(TIME_STEP) != -1) {
             if (wallfound) {
                 if (positionParallelToWall()) {
@@ -58,7 +53,7 @@ public class BB_WallGrind {
     }
 
     public boolean wallfound() {
-        if (smooth(robot.getDSSensor("NNO").getValue()) > 50 && robot.getDSSensor("NNW").getValue() > 50) {
+        if (smooth(robot.getDSSensor("NNO").getValue()) > 50 && smooth(robot.getDSSensor("NNW").getValue()) > 50) {
             return true;
         }
         return false;
